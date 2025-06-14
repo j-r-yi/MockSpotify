8000962231bf4cc7d6742730422731c6699cebe6
Joshua Yi <jryi@scu.edu>
2025-06-14 14:43:54 -0700

    section model and networkapi

---
+++ app/src/main/java/com/laioffer/spotify/network/NetworkApi.kt
@@ -0,0 +1,12 @@
+package com.laioffer.spotify.network
+
+import com.laioffer.spotify.datamodel.Section
+import retrofit2.Call
+import retrofit2.http.GET
+
+// definition of spotify backend api call
+
+interface NetworkApi {
+    @GET("feed")
+    fun getHomeFeed(): Call<List<Section>>
+}
