0b1208b85f004a9142a4510c0922eefbd23d3308
Joshua Yi <jryi@scu.edu>
2025-06-18 15:52:25 +0800

    use of hilt

--- app/src/main/java/com/laioffer/spotify/MainActivity.kt
+++ app/src/main/java/com/laioffer/spotify/MainActivity.kt
@@ -1,88 +1,61 @@
 package com.laioffer.spotify
 
-import android.media.Image
 import android.os.Bundle
 import android.util.Log
-import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.OutlinedTextField
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import coil.compose.AsyncImage
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
 import coil.compose.AsyncImage
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import com.laioffer.spotify.ui.theme.SpotifyTheme
+import com.laioffer.spotify.datamodel.Section
+import com.laioffer.spotify.network.NetworkApi
+import com.laioffer.spotify.network.NetworkModule
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import retrofit2.Call
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 // customized extend AppCompatActivity
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var api: NetworkApi
+
     private val TAG = "lifecycle"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Log.d(TAG, "We are at onCreate()")
-
-        // Compose
-//        setContent {
-//            SpotifyTheme {
-//                // A surface container using the 'background' color from the theme
-//                Surface(
-//                    modifier = Modifier.fillMaxSize(),
-//                    color = MaterialTheme.colors.background
-//                ) {
-////                    Greeting("Android")
-////                    HelloContent()
-////                    var name by remember {
-////                        mutableStateOf("")
-////                    }
-////                    HelloContentStateless(
-////                        name = name,
-////                        onNameChange = { name = it}
-////                    )
-//                    AlbumCover()
-//                }
-//            }
-//        }
         setContentView(R.layout.activity_main)
-
-
-        // nav_graph, navhost, navcontroller
 
         val navView = findViewById<BottomNavigationView>(R.id.nav_view)
 
         // navHost, navController
         // using navController to change the fragment in navHost
 
-        val navHostFragment =supportFragmentManager
+        val navHostFragment = supportFragmentManager
             .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-
         val navController = navHostFragment.navController
         navController.setGraph(R.navigation.nav_graph)
 
+        // let bottom nav navigate
         NavigationUI.setupWithNavController(navView, navController)
 
         // https://stackoverflow.com/questions/70703505/navigationui-not-working-correctly-with-bottom-navigation-view-implementation
@@ -92,159 +65,44 @@ class MainActivity : AppCompatActivity() {
             true
         }
 
+        // Test Retrofit
+        val retrofit = NetworkModule.provideRetrofit()
+        val api: NetworkApi = retrofit.create(NetworkApi::class.java)
+        val responseCall: Call<List<Section>> = api.getHomeFeed()
+
+        GlobalScope.launch(Dispatchers.IO) {
+            val response = responseCall.execute()
+            val sections = response.body()
+            Log.d("Network", sections.toString())
+        }
     }
 }
 
 @Composable
 fun AlbumCover() {
     Column {
-        Box (modifier = Modifier.size(160.dp)) {
-            // https://upload.wikimedia.org/wikipedia/en/d/d1/Stillfantasy.jpg
+        Box(modifier = Modifier.size(160.dp)) {
+            // "https://upload.wikimedia.org/wikipedia/en/d/d1/Stillfantasy.jpg"
 //            Image(painter = , contentDescription = )
             AsyncImage(
                 model = "https://upload.wikimedia.org/wikipedia/en/d/d1/Stillfantasy.jpg",
                 contentDescription = null,
                 modifier = Modifier.fillMaxSize(),
-                contentScale = ContentScale.FillBounds
-            )
+                contentScale = ContentScale.FillBounds)
             Text(
                 text = "Still Fantasy",
+                color = Color.White,
                 modifier = Modifier
-                    .padding(start = 2.dp, bottom = 4.dp) // left is start, right is end
-                    .align(Alignment.BottomStart),
-                style = MaterialTheme.typography.body2.copy(fontWeight = FontWeight.Bold),
-                color = Color.White
+                    .padding(bottom = 4.dp, start = 2.dp)
+                    .align(Alignment.BottomStart)
             )
         }
-
         Text(
             text = "Jay Chou",
             modifier = Modifier.padding(top = 4.dp, start = 2.dp),
-            style = MaterialTheme.typography.body2.copy(fontWeight = FontWeight.Bold),
-            color = Color.LightGray
+            color = Color.LightGray,
+            // kotlin data class
+            style = MaterialTheme.typography.body2.copy(fontWeight = FontWeight.Bold)
         )
-    }
-}
-
-@Composable
-fun ArtistCardBox() {
-    Box {
-        Text("Alfred Sisley")
-        Text("3 minutes ago")
-    }
-}
-
-@Composable
-fun ArtistCardColumn() {
-    Column {
-        Text("Alfred Sisley")
-        Text("3 minutes ago")
-    }
-}
-
-@Composable
-fun ArtistCardRow() {
-    Row {
-        Text("Alfred Sisley")
-        Text("3 minutes ago")
-    }
-}
-
-@Preview
-@Composable
-fun PreviewArtistCardBox() {
-    SpotifyTheme {
-        Surface {
-            ArtistCardBox()
-        }
-    }
-}
-
-@Preview
-@Composable
-fun PreviewArtistCardRow() {
-    SpotifyTheme {
-        Surface {
-            ArtistCardRow()
-        }
-    }
-}
-
-@Preview
-@Composable
-fun PreviewArtistCardColumn() {
-    SpotifyTheme {
-        Surface {
-            ArtistCardColumn()
-        }
-    }
-}
-
-
-@Composable
-private fun Greeting(name: String) {
-    Column(modifier = Modifier
-        .fillMaxWidth()
-        .wrapContentHeight()
-//        .padding(24.dp)
-        .background(Color.Yellow)
-        .padding(24.dp)
-
-    ) {
-        Text(text = "Hello,")
-        Text(text = name)
-    }
-}
-
-@Composable
-fun HelloContent() {
-//    var name: String = "" // state
-    // by: delegation
-    var name by remember { mutableStateOf("") } // state
-
-    Column {
-        if (name.isNotEmpty()) {
-            Text(
-                modifier = Modifier.padding(bottom = 8.dp),
-                text = "Hello! $name",
-                style = MaterialTheme.typography.body2
-            )
-        }
-        OutlinedTextField(
-            value = name,
-            onValueChange = {
-                name = it
-            },
-            label = { Text(text = "name") }
-        )
-    }
-}
-
-@Composable
-fun HelloContentStateless(name: String, onNameChange: (String) -> Unit) {
-    Column {
-        if (name.isNotEmpty()) {
-            Text(
-                modifier = Modifier.padding(bottom = 8.dp),
-                text = "Hello! $name",
-                style = MaterialTheme.typography.body2
-            )
-        }
-        OutlinedTextField(
-            value = name,
-            onValueChange = onNameChange,
-            label = { Text(text = "name") }
-        )
-    }
-}
-
-
-@Preview(showBackground = true)
-@Composable
-fun PreviewGreeting() {
-    SpotifyTheme {
-        Surface {
-            HelloContent()
-        }
     }
 }
