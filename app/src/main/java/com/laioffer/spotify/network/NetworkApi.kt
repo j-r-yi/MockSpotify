517cc573a983b669851551426e0002c8e2addf67
Joshua Yi <jryi@scu.edu>
2025-06-22 17:00:13 +0800

    implementation of playlist viewmodel and repository

--- app/src/main/java/com/laioffer/spotify/network/NetworkApi.kt
+++ app/src/main/java/com/laioffer/spotify/network/NetworkApi.kt
@@ -1,12 +1,19 @@
 package com.laioffer.spotify.network
 
+import com.laioffer.spotify.datamodel.Playlist
 import com.laioffer.spotify.datamodel.Section
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 // definition of spotify backend api call
-
 interface NetworkApi {
+    // http://0.0.0.0:8080/feed
     @GET("feed")
     fun getHomeFeed(): Call<List<Section>>
+
+    // http://0.0.0.0:8080/playlist/{id}
+    @GET("playlist/{id}")
+    fun getPlaylist(@Path("id") id: Int): Call<Playlist>
+
 }
