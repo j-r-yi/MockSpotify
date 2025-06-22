b8149983563908fb1f948f57905c4a143d8d4be2
Joshua Yi <jryi@scu.edu>
2025-06-22 11:24:10 +0800

    add on click events

--- app/src/main/java/com/laioffer/spotify/ui/home/HomeFragment.kt
+++ app/src/main/java/com/laioffer/spotify/ui/home/HomeFragment.kt
@@ -1,6 +1,7 @@
 package com.laioffer.spotify.ui.home
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -24,7 +25,10 @@ class HomeFragment : Fragment() {
         return ComposeView(requireContext()).apply {
             setContent {
                 MaterialTheme(colors = darkColors()){
-                    HomeScreen(viewModel)
+                    HomeScreen(viewModel, onTap = {
+                        Log.d("HomeFragment", "We tapped on album: ${it.name}")
+
+                    })
                 }
             }
         }
