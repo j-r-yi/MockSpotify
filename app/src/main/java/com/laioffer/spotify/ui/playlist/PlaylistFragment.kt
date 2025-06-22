be6a52379652976eae2a36296c416b9333b2512d
Joshua Yi <jryi@scu.edu>
2025-06-22 16:02:45 +0800

    initialize album playlist fragment

---
+++ app/src/main/java/com/laioffer/spotify/ui/playlist/PlaylistFragment.kt
@@ -0,0 +1,33 @@
+package com.laioffer.spotify.ui.playlist
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.ui.platform.ComposeView
+import androidx.fragment.app.Fragment
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class PlaylistFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        return ComposeView(requireContext()).apply {
+            setContent {
+                MaterialTheme(colors = darkColors()) {
+                }
+            }
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
