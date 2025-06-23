5fb244b67dea86bc5410a39047b10f3016759d06
Joshua Yi <jryi@scu.edu>
2025-06-23 20:57:31 +0800

    implementation of database dao and module

---
+++ app/src/main/java/com/laioffer/spotify/database/DatabaseDao.kt
@@ -0,0 +1,22 @@
+package com.laioffer.spotify.database
+
+import androidx.room.*
+import com.laioffer.spotify.datamodel.Album
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface DatabaseDao {
+
+    // create and update
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun favoriteAlbum(album: Album)
+
+    // read
+    @Query("SELECT EXISTS(SELECT * FROM Album WHERE id = :id)")
+    fun isFavoriteAlbum(id : Int) : Flow<Boolean>
+
+
+    // delete
+    @Delete
+    fun unFavoriteAlbum(album: Album)
+}
